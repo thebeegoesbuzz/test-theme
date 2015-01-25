9832d8c05a320c7cce09eac590a5680a4e5b6e4e
Pete Burgess <peteburgess@gmail.com>
2015-01-24 23:14:00 -0500

    Simplified the loop
    no need for anything complicated yet

--- index.php
+++ index.php
@@ -24,35 +24,16 @@
 		
 		<section id="main">
 			
-			<?php if ( have_posts() ) : ?>
-
-			<?php if ( is_home() && ! is_front_page() ) : ?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-			<?php endif; ?>
-
 			<?php
-			// Start the loop.
-			while ( have_posts() ) : the_post();
 
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'content', get_post_format() );
+if (have_posts()) :
+   while (have_posts()) :
+      the_post();
+         the_content();
+   endwhile;
+endif;
 
-			// End the loop.
-			endwhile;
-
-		// If no content, include the "No posts found" template.
-		else :
-			get_template_part( 'content', 'none' );
-
-		endif;
-		?>
-		
+?>		
 		</section><!-- /main -->
 		
 		<footer>
