7d0ced72f0999b749b081b7432dfec8683b35bff
Pete Burgess <peteburgess@gmail.com>
2015-01-27 22:11:13 -0500

    A little more clean up
    Removed some unnecessary remnants of my default page code.

--- index.php
+++ index.php
@@ -3,11 +3,8 @@
 <head>
 	<meta charset="utf-8">
 	<title><?php bloginfo('name'); ?></title>
-	<meta name="description" content="">
-	<meta name="keywords" content="" />
-	<meta name="author" content="">
 	<meta name="viewport" content="width=device-width; initial-scale=1.0">
-	<link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/style.css?v=1">
+	<link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/style.css">
 	<!--[if lt IE 9]>
 	<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
 	<![endif]-->
